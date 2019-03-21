5cb4137904320db993f5ed2a777de44201a1bcc1
Garnik Gasparyan <gasp.garnik@gmail.com>
2019-03-21 23:18:46 +0400

    [WIP] Change js default behavior
    If field is not populated then js replaces it with 'undefined'
    value, which can't be detected in not js language.
    Not populated fields need to be empty and not otherwise!

--- public/js/angular/authentication/common-services/authentication-form-nerd.js
+++ public/js/angular/authentication/common-services/authentication-form-nerd.js
@@ -1,10 +1,12 @@
 TableOrder.service("AuthenticationFormNerd", [function() {
     this.getSignUpFormData = function(scope) {
         var objectToReturn = {};
-        objectToReturn.firstName = scope.firstName;
-        objectToReturn.lastName = scope.lastName;
-        objectToReturn.username = scope.username;
-        objectToReturn.password = scope.password;
+        objectToReturn.firstName = scope.firstName === undefined ? "" : scope.firstName;
+        objectToReturn.lastName = scope.lastName === undefined ? "" : scope.lastName;
+        objectToReturn.username = scope.username === undefined ? "" : scope.username;
+        objectToReturn.password = scope.password === undefined ? "" : scope.password;
+
+        console.log(objectToReturn);
 
         return objectToReturn;
     }
