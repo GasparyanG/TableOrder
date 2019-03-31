72bb0ef1635f3be5cb06e8735b177892d24ff755
Garnik Gasparyan <gasp.garnik@gmail.com>
2019-03-31 14:45:37 +0400

    Add comment supplier

---
+++ src/Service/Comment/CommentSupplierInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Service\Comment;
+
+interface CommentSupplierInterface
+{
+    public function getCommentFromPhpInputFile(): string;
+}
---
+++ src/Service/Comment/Products/CommentSupplier.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Service\Comment\Products;
+
+use App\Service\Comment\CommentFetcher\CommentFetcherInterface;
+use App\Service\Comment\CommentSupplierInterface;
+
+class CommentSupplier implements CommentSupplierInterface
+{
+    private $commentFetcher;
+
+    public function __construct(CommentFetcherInterface $commentFetcher)
+    {
+        $this->commentFetcher = $commentFetcher;
+    }
+
+    public function getCommentFromPhpInputFile(): string
+    {
+        return $this->commentFetcher->getCommentFromPhpInputFile();
+    }
+}
