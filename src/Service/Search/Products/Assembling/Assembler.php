33382d64244f13a6d1c4affb1b3a8651d65a836a
Garnik Gasparyan <gasp.garnik@gmail.com>
2019-04-02 15:41:11 +0400

    Assemble tables by adding more info about restaurant
    [Assembler] restaurant info need to be taken explicitly.

--- src/Service/Search/Products/Assembling/Assembler.php
+++ src/Service/Search/Products/Assembling/Assembler.php
@@ -2,12 +2,19 @@
 
 namespace App\Service\Search\Products\Assembling;
 
+use App\Service\Restaurant\RestaurantData\SingleRestaurantDataPreparingInterface;
 use App\Service\Search\Products\Assembling\AssemblerInterface;
 
 class Assembler implements AssemblerInterface
 {
     // every restaurant will display at most this amount of tables
     private $amountOfTablesForRestaurant = 6;
+    private $singleRestaurantDataPreparing;
+
+    public function __construct(SingleRestaurantDataPreparingInterface $singleRestaurantDataPreparing)
+    {
+        $this->singleRestaurantDataPreparing = $singleRestaurantDataPreparing;
+    }
 
     /**
      * @param array $notReservedTables array of RestaurantTable objects
@@ -20,14 +27,15 @@ class Assembler implements AssemblerInterface
         foreach($notReservedTables as $table) {
             $resName = $table->getRestaurant()->getName();
             if (isset($arrayToBeReturned[$resName])) {
-                if (count($arrayToBeReturned[$resName]) < 6) {
-                    $arrayToBeReturned[$resName][] = $table;
+                if (count($arrayToBeReturned[$resName]["tables"]) < 6) {
+                    $arrayToBeReturned[$resName]["tables"][] = $table;
                 }
             }
 
             else {
                 // this will automatically set key for array
-                $arrayToBeReturned[$resName][] = $table;
+                $arrayToBeReturned[$resName]["tables"][] = $table;
+                $arrayToBeReturned[$resName]["restaurant"]["restaurant"] = $this->singleRestaurantDataPreparing->populateRestaurant($table->getRestaurant());
             }
         }
 
