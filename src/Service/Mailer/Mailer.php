1e85c27483e996ed61e12079bd2f015fab2a7369
Garnik Gasparyan <gasp.garnik@gmail.com>
2019-04-03 22:57:40 +0400

    Get authenticated user
    To make email more polite user data is required.

--- src/Service/Mailer/Mailer.php
+++ src/Service/Mailer/Mailer.php
@@ -4,17 +4,27 @@ namespace App\Service\Mailer;
 
 use App\Service\Mailer\Authentication\Verification\VerificationMailerInterface;
 
+use App\Entity\Reservation;
+use App\Service\Mailer\Reservation\ReservationMailerInterface;
+
 class Mailer implements MailerInterface
 {
     private $verificationMailer;
+    private $reservationMailer;
 
-    public function __construct(VerificationMailerInterface $verificationMailer)
+    public function __construct(VerificationMailerInterface $verificationMailer, ReservationMailerInterface $reservationMailer)
     {
         $this->verificationMailer = $verificationMailer;
+        $this->reservationMailer = $reservationMailer;
     }
 
     public function sendVerificationCode(string $email): void
     {
         $this->verificationMailer->sendVerificationCode($email);
+    }
+
+    public function sendReservationDetailsToUser(Reservation $reservation): void
+    {
+        $this->reservationMailer->sendReservationDetailsToUser($reservation);
     }
 }
--- src/Service/Mailer/MailerInterface.php
+++ src/Service/Mailer/MailerInterface.php
@@ -2,7 +2,11 @@
 
 namespace App\Service\Mailer;
 
+use App\Entity\Reservation;
+
 interface MailerInterface
 {
     public function sendVerificationCode(string $email): void;
+
+    public function sendReservationDetailsToUser(Reservation $reservation): void;
 }
