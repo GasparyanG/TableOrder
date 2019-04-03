afcd68759ba2433428b296ae1ac465eec8a1f45e
Garnik Gasparyan <gasp.garnik@gmail.com>
2019-04-03 23:05:43 +0400

    Create reservation details view for email

---
+++ templates/mailing_views_php/reservation/reservation_details.html.php
@@ -0,0 +1,27 @@
+<html>
+    <body>
+        <h3>Dear <?= $user->getFirstName() . " " . $user->getLastName() ?> here is your reservation details</h3>
+        <div class="restaurant">
+            <h4>Restaurant</h4>
+            <span>Restaurant Name: </span><?= $reservation->getRestauran()->getName() ?>
+
+            <br>
+            <span>Restaurant Location: </span> <?= $reservation->getRestauran()->getLocation() ?>
+        </div>
+
+        <div class="Reservation">
+            <h4>Reservation</h4>
+            <span>Time And Date: </span> <?= $reservation->getReservationTime()->format("H:i:s") . " " . $reservation->getReservationTime()->format("Y-m-d") ?>
+
+            <br>
+            <span>Amount of Time: </span> <?= $reservation->getAmountOfTime() . " minutes"?>
+
+            <br>
+            <span>Table Number: </span> <?= $reservation->getTableNumber() ?>
+        </div>
+
+        <div>
+            <p>For more details visit <a href="https://www.gasparyan.com/me">your page in TableOrder</a></p>
+        </div>
+    </body>
+</html>
