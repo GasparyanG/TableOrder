fb1cb62e5865c32a769e0f156ea4576354f949ab
Garnik Gasparyan <gasp.garnik@gmail.com>
2019-04-07 11:40:11 +0400

    Include client data with ClientDataComposerInterface
    Because home page in its view holds search form, its meaningless to
    render search button in nav bar.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,28 +2,18 @@
 
 namespace App\Controller;
 
+use App\Service\BaseLayout\ClientDataComposerInterface;
+use App\Service\ConfigurationFetcher\Templating\TemplatingConfigFetcherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Form\Search\Interfaces\SearchFormInterface;
-use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
-    public function getPage(SearchFormInterface $formHelper, Request $request)
+    public function getPage(ClientDataComposerInterface $clientDataComposer,
+                            TemplatingConfigFetcherInterface $templatingConfigFetcher)
     {
-        /**
-         * 1) Request is needed to get queryString [x]
-         * 2) psr container is needed to instantiate serrvice [x] already done in factory object
-         * 3) run dispatcher to instantiate required abstract fatory
-         * 4) getValidator()
-         * 5) checkTime()
-         * 6) sanitizeQueiryParams()
-         * 7) validateQueryParams()
-         * 8) getNotReservedTables()
-        */
+        $dataForClient = $clientDataComposer->composeData();
+        $dataForClient[$templatingConfigFetcher->getSearchState()] = false;
 
-        return $this->render("home/main.html.twig", [
-            "cities" => $formHelper->getLocationsForChoiceType(),
-            "arrayOfPersonAmounts" => $formHelper->getPersonAmountArray()
-        ]);
+        return $this->render("home/main.html.twig", $dataForClient);
     }
 }
